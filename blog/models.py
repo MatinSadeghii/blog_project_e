14698248808a2ceb473c68d7262fd55ba940d2e3
MatinSadeghii <byteboss2007@gmail.com>
2025-01-26 20:31:50 +0330

    models.py updated

--- blog/models.py
+++ blog/models.py
@@ -3,12 +3,26 @@ from django.utils import timezone
 # Create your models here.
 
 class Post(models.Model):
+
+    class PublishManager(models.Manager):
+        def get_queryset(self):
+            return super().get_queryset().filter(staus=Post.Staus.PUBLISHED)
+
+    class Staus(models.TextField):
+        PUBLISHED = 'PB','Publish'
+        DRAFT = 'DR','Draft'
+
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250)
     body = models.TextField()
     publish = models.DateTimeField(default=timezone)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+
+    staus = models.CharField(choices=Staus.choices,max_length=2,default=Staus.DRAFT)
+
+    objects = models.Manager()
+    published = PublishManager()
 
     class Meta:
         ordering = ['-publish']
